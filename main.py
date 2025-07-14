0be626f34c0ce84a60d9420e6cbf25cea5382fc9
iDiv3r <udhgjkf@gmail.com>
2025-07-14 12:39:00 -0600

    Conexión con Laravel
    Agregar passlib a requirements.txt
    
    Cambiar allow origins en main.py para permitir todas las solicitudes de origen

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI(title="API de Recolección")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5000", "http://localhost:5000"],  # Laravel
+    # allow_origins=["http://127.0.0.1:5000", "http://localhost:5000"],  # Laravel
+    allow_origins=["*"],  # Permitir todas las solicitudes de origen
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
